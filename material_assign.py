db06f35205b20ba44d44ac417e830d07ecb382c1
Alex Schweiger <alexjschweiger@gmail.com>
2024-12-04 21:34:09 -0600

    Added __init__ for testing, not sure if I fully understand how to finalize a blender addon yet, however the code works in isolation in Blender.

--- material_assign.py
+++ material_assign.py
@@ -34,6 +34,8 @@ class AssignScreenMaterial():
     bl_options = {'REGISTER', 'UNDO'}
     screen_num = 0
 
+    def __init__(self):
+        self.create_material
 
     def check_material_existance(self):
         exists = False
@@ -51,22 +53,33 @@ class AssignScreenMaterial():
         #Creates additional material every time function is run to avoid issues, may be unnecessary as blender already does this.
         self.setup_material(screen_mat)
     
-    def setup_material(screen_mat, self):
+    def setup_material(self, screen_mat):
         screen_mat.use_nodes = True
         node_tree = screen_mat.node_tree
         screen_nodes = node_tree.nodes
+        self.create_nodes(screen_nodes)
+        self.set_location(self.node_list)
+
+    def create_nodes( self, screen_nodes):
         bsdf_node = screen_nodes.get('Principled BSDF')
         output_node = screen_nodes.get('Material Output')
         img_node = screen_nodes.new('ShaderNodeTexImage')
+        sep_rgb_node_img = screen_nodes.new('ShaderNodeSeparateColor')
         pixel_node = screen_nodes.new('ShaderNodeTexImage')
+        sep_rgb_node_pix = screen_nodes.new('ShaderNodeSeparateColor')
+        math_node = {
+            "r" : screen_nodes.new('ShaderNodeMath'),
+            "g" : screen_nodes.new('ShaderNodeMath'),
+            "b" : screen_nodes.new('ShaderNodeMath'),
+        }
         mapping_node = screen_nodes.new('ShaderNodeMapping')
         coord_node = screen_nodes.new('ShaderNodeTexCoord')
 
-        node_list = [output_node, bsdf_node, img_node, pixel_node, mapping_node, coord_node]
+        node_list = [output_node, bsdf_node, img_node, pixel_node, math_node["r"], math_node["b"], math_node["g"], mapping_node, coord_node]
 
-        self.set_locations(node_list)
 
-    def set_location(node_list):
+
+    def set_location(self, node_list):
         x_pos = 200
         for node in node_list:
             node.location = (x_pos, 0)
@@ -78,16 +91,20 @@ class AssignScreenMaterial():
         
 
 
-    def assign_material(obj, self):
+  
         
 
 
-     def execute(self):
+    def execute(self):
+        self.create_material
         
 
         return {'FINISHED'}
     
 
+
+
+AssignScreenMaterial
 
 def register():
     bpy.utils.register_class(AssignScreenMaterial)
