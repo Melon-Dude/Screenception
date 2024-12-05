1551c1871ed616bafafb6678f0e63a57835e9843
Alex Schweiger <alexjschweiger@gmail.com>
2024-12-04 23:54:02 -0600

    All nodes route properly, needed to rework math nodes.

--- material_assign.py
+++ material_assign.py
@@ -63,23 +63,39 @@ class ScreenMaterial():
         nodes = {
         'output_node' : screen_nodes.get('Material Output'),
         'bsdf_node' : screen_nodes.get('Principled BSDF'),
-        'r' : self.mult_template(screen_nodes),
-        'g' : self.mult_template(screen_nodes),
-        'b' : self.mult_template(screen_nodes),
+        'combine_rgb' : screen_nodes.new('ShaderNodeCombineColor'),
+        'r' : screen_nodes.new('ShaderNodeMath'),
+        'g' : screen_nodes.new('ShaderNodeMath'),
+        'b' : screen_nodes.new('ShaderNodeMath'),
         'img_node' : screen_nodes.new('ShaderNodeTexImage'),
         'sep_rgb_node_img' : screen_nodes.new('ShaderNodeSeparateColor'),
         'pixel_node' : screen_nodes.new('ShaderNodeTexImage'),
         'sep_rgb_node_pix' : screen_nodes.new('ShaderNodeSeparateColor'),
-        'mapping_node' : screen_nodes.new('ShaderNodeMapping'),
+        'img_mapping_node' : screen_nodes.new('ShaderNodeMapping'),
+        'pix_mapping_node' : screen_nodes.new('ShaderNodeMapping'),
         'coord_node' : screen_nodes.new('ShaderNodeTexCoord')
         }
         #if self.is_crt:
             #crt_tex = screen_nodes.new("ShaderNodeTexWave")
         
         links = node_tree.links
-        links.new(nodes['img_node'].outputs['Color'], nodes['bsdf_node'].inputs[0])
+        links.new(nodes['coord_node'].outputs['UV'], nodes['img_mapping_node'].inputs[0])
+        links.new(nodes['img_mapping_node'].outputs['Vector'], nodes['img_node'].inputs[0])
+        links.new(nodes['coord_node'].outputs['UV'], nodes['pix_mapping_node'].inputs[0])
+        links.new(nodes['pix_mapping_node'].outputs['Vector'], nodes['pixel_node'].inputs[0])
+        links.new(nodes['img_node'].outputs['Color'], nodes['sep_rgb_node_img'].inputs[0])
+        links.new(nodes['sep_rgb_node_img'].outputs['Red'], nodes['r'].inputs[0])
+        links.new(nodes['sep_rgb_node_img'].outputs['Blue'], nodes['b'].inputs[0])
+        links.new(nodes['sep_rgb_node_img'].outputs['Green'], nodes['g'].inputs[0])
+        links.new(nodes['pixel_node'].outputs['Color'], nodes['sep_rgb_node_pix'].inputs[0])
+        links.new(nodes['sep_rgb_node_pix'].outputs['Red'], nodes['r'].inputs[1])
+        links.new(nodes['sep_rgb_node_pix'].outputs['Blue'], nodes['b'].inputs[1])
+        links.new(nodes['sep_rgb_node_pix'].outputs['Green'], nodes['g'].inputs[1])
+        links.new(nodes['r'].outputs['Value'], nodes['combine_rgb'].inputs[0])
+        links.new(nodes['r'].outputs['Value'], nodes['combine_rgb'].inputs[1])
+        links.new(nodes['r'].outputs['Value'], nodes['combine_rgb'].inputs[2])
+        links.new(nodes['combine_rgb'].outputs['Color'], nodes['bsdf_node'].inputs[28])
 
-        self.link_nodes(nodes, node_tree)
         self.set_location(nodes)
 
 
